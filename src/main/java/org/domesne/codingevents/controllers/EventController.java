137bfd8646aabc11bd0dbbb2a040163d9e532488
KCCoderGirl <dontlaughatme@yahoo.com>
2022-05-17 20:47:40 -0500

    exercises complete

--- src/main/java/org/domesne/codingevents/controllers/EventController.java
+++ src/main/java/org/domesne/codingevents/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,12 +21,14 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
+        model.addAttribute("title","All Events");
         model.addAttribute("events", EventData.getAllEvents());
         return "events/index";
     }
 
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
         return "events/create";
     }
 
@@ -37,6 +40,7 @@ public class EventController {
 
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model){
+        model.addAttribute("title","Delete Events");
         model.addAttribute("events",EventData.getAllEvents());
         return "events/delete";
     }
@@ -48,6 +52,22 @@ public class EventController {
                 EventData.removeEvent(id);
             }
         }
+        return "events";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId){
+        Event editEvent = EventData.getEventById(eventId);
+        model.addAttribute("title","Edit Event "+editEvent.getName()+" (id="+editEvent.getId()+")");
+        model.addAttribute("event",editEvent);
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String editEvent(int eventId,String name,String description){
+            Event editEvent = EventData.getEventById(eventId);
+            editEvent.setName(name);
+            editEvent.setDescription(description);
         return "redirect:";
     }
 }
